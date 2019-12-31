5a73a3aac26eedc46e6d61b61637d284a7d847a0
bwettenstein <wettenstein@gmail.com>
2019-12-30 20:11:29 -0800

    Added icons appearing when button is clicked

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ const newGameButton = document.getElementById("new-game");
 const rockButton = document.getElementById("Rock");
 const paperButton = document.getElementById("Paper");
 const scissorsButton = document.getElementById("Scissors");
+let playerScoreReport = document.getElementById("pScore");
+let cpuScoreReport = document.getElementById("cpuScore");
+let playerChosenIcon = document.getElementById("playerChosenIcon");
+let computerChosenIcon = document.getElementById("cpuChosenIcon");
 let playerScore = 0;
 let computerScore = 0;
 let rounds = 0;
@@ -33,45 +37,81 @@ const newGame = () => {
     playerScore = 0;
     computerScore = 0;
     rounds = 0;
+    playerScoreReport.innerHTML = playerScore;
+    cpuScoreReport.innerHTML = computerScore;
+    playerChosenIcon.innerHTML = "";
+    computerChosenIcon.innerHTML = "";
 }
 
 // Gets the choices from both players and then calls functions based on the outcomes
 let playGame = (playerChoice, computerChoice) =>{
     let choices = playerChoice+computerChoice;
-    alert(choices);
-    switch(choices) {
-        case "RockScissors":
-        case "PaperRock":
-        case "ScissorsPaper":
-            win(playerChoice, computerChoice);
-            break;
-        case "ScissorsRock":
-        case "RockPaper":
-        case "PaperScissors":
-            loss(playerChoice, computerChoice);
-            break;
-        case "RockRock":
-        case "PaperRock":
-        case "ScissorsPaper":
-            draw(playerChoice, computerChoice);
-            break;    
+    switch(playerChoice) {
+        case "Rock":
+            displayPlayerIcon(playerChoice);
+        case "Paper":
+            displayPlayerIcon(playerChoice);
+        case "Scissors":
+            displayPlayerIcon(playerChoice);
     }
+    // switch(choices) {
+    //     case "RockScissors":
+    //     case "PaperRock":
+    //     case "ScissorsPaper":
+    //         win(playerChoice, computerChoice);
+    //         break;
+    //     case "ScissorsRock":
+    //     case "RockPaper":
+    //     case "PaperScissors":
+    //         loss(playerChoice, computerChoice);
+    //         break;
+    //     case "RockRock":
+    //     case "PaperRock":
+    //     case "ScissorsPaper":
+    //         draw(playerChoice, computerChoice);
+    //         break;    
+    // }
 }
 
 // Function that increments the player score and number of rounds elapsed after winning. ADD WRITING TO HTML WHEN FINISHED
 const win = (playerChoice, computerChoice) => {
     playerScore++;
     rounds++;
+    playerScoreReport.innerHTML = playerScore;
 }
 
 const loss = (playerChoice, computerChoice) => {
     computerScore++;
     rounds++;
+    cpuScoreReport.innerHTML = computerScore;
 }
 
 const draw = (playerChoice, computerChoice) => {
     rounds++;
 }
 
-let message = () => alert("Hello there");
+let displayPlayerIcon = (playerChoice) => {
+    if (playerChoice === "Rock") {
+        playerChosenIcon.innerHTML = "<i class='fas fa-hand-rock' alt='rock' id='playerIcon'></i>";
+    }
+    else if (playerChoice === "Paper") {
+        playerChosenIcon.innerHTML = "<i class='fas fa-hand-paper' alt='paper' id='playerIcon'></i>";
+    }
+    else {
+        playerChosenIcon.innerHTML = "<i class='fas fa-hand-scissors' alt='scissors' id='playerIcon'></i>";
+    }
+}
+
+let displayCpuIcon = (computerChoice) => {
+    if (computerChoice === "Rock") {
+        computerChoice.innerHTML = "<i class='fas fa-hand-rock' alt='rock' id='cpuIcon'></i>";
+    }
+    else if (computerChoice === "Paper") {
+        computerChosenIcon.innerHTML = "<i class='fas fa-hand-paper' alt='paper' id='cpuIcon'></i>";
+    }
+    else {
+        computerChosenIcon.innerHTML = "<i class='fas fa-hand-scissors' alt='scissors' id='cpuIcon'></i>";
+    }
+} 
+
 newGameButton.addEventListener("click", newGame);
